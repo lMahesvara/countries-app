e0c71d859a4d1184e8d987664a5c128cc065c92f
lMahesvara <erick082016@gmail.com>
2022-08-24 17:12:04 -0700

    The option 'filter by region' can be used now

--- src/components/filters/RegionFilter.js
+++ src/components/filters/RegionFilter.js
@@ -1,19 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 
 const RegionFilter = ({ setFilter, filter }) => {
+  const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"]
+
+  const handleChange = (e) => {
+    setFilter({ ...filter, region: e.target.value })
+    if (e.target.value === "" && e.target.selectedIndex === 0) return
+
+    //make the option Filter by region hidden when the user selects another option
+    e.target.options[0].hidden = true
+    e.target.options[0].disabled = true
+
+    //make the option All regions visible
+    e.target.options[1].hidden = false
+    e.target.options[1].disabled = false
+  }
+
   return (
     <div className="select-bg">
-      <select
-        name="region"
-        onChange={(e) =>
-          setFilter({ ...filter, [e.target.name]: e.target.value })
-        }
-      >
-        <option value="" disabled hidden>
-          Filter by Region...
+      <select name="region" onChange={(e) => handleChange(e)}>
+        <option value="">Filter by Region...</option>
+        <option value="" hidden disabled>
+          All Regions
         </option>
-        <option value="">All Regions</option>
-        {["Africa", "Americas", "Asia", "Europe", "Oceania"].map((el) => (
+        {regions.map((el) => (
           <option key={el} value={el}>
             {el}
           </option>
